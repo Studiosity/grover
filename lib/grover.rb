7356b11721d0d512eb794bfef7c1af768fedd54e
Koen Handekyn <koen.handekyn@up-nxt.com>
2019-06-19 09:05:27 +0200

    running on heroku: readme and improvement to ENV name for enabled no-sandbox mode

--- lib/grover.rb
+++ lib/grover.rb
@@ -23,7 +23,7 @@ class Grover
     dependencies puppeteer: 'puppeteer'
 
     def self.launch_params
-      ENV['CI'] == 'true' ? "{args: ['--no-sandbox', '--disable-setuid-sandbox']}" : '{}'
+      ENV['GROVER_NO_SANDBOX'] == 'true' ? "{args: ['--no-sandbox', '--disable-setuid-sandbox']}" : '{}'
     end
 
     method :convert_pdf, Utils.strip_heredoc(<<-FUNCTION)
