9e31e41b1373ad397a9194dbf83f3cafbfd75e50
Andrew Bromwich <a.bromwich@gmail.com>
2020-06-23 01:28:07 +1000

    Address Rubocop Style/RedundantRegexpEscape lints (#69)

--- lib/grover/html_preprocessor.rb
+++ lib/grover/html_preprocessor.rb
@@ -17,13 +17,13 @@ class Grover
 
     def self.translate_relative_paths(html, root_url)
       # Try out this regexp using rubular http://rubular.com/r/hiAxBNX7KE
-      html.gsub(%r{(href|src)=(['"])/([^/"']([^\"']*|[^"']*))?['"]}, "\\1=\\2#{root_url}\\3\\2")
+      html.gsub(%r{(href|src)=(['"])/([^/"']([^"']*|[^"']*))?['"]}, "\\1=\\2#{root_url}\\3\\2")
     end
     private_class_method :translate_relative_paths
 
     def self.translate_relative_protocols(body, protocol)
       # Try out this regexp using rubular http://rubular.com/r/0Ohk0wFYxV
-      body.gsub(%r{(href|src)=(['"])//([^\"']*|[^"']*)['"]}, "\\1=\\2#{protocol}://\\3\\2")
+      body.gsub(%r{(href|src)=(['"])//([^"']*|[^"']*)['"]}, "\\1=\\2#{protocol}://\\3\\2")
     end
     private_class_method :translate_relative_protocols
   end
