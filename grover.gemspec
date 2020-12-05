6fbc55b8d7262a00a8d3cb984614cd8d278143f8
Andrew Bromwich <a.bromwich@gmail.com>
2020-12-05 22:30:42 +1000

    Add handling of invalid worker responses (#92)

--- grover.gemspec
+++ grover.gemspec
@@ -18,7 +18,7 @@ Gem::Specification.new do |spec|
   spec.license     = 'MIT'
   spec.required_ruby_version = ['>= 2.5.0', '< 2.8.0']
 
-  spec.files         = `git ls-files lib`.split("\n")
+  spec.files         = `git ls-files lib`.split("\n") + ['LICENSE']
   spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
   spec.test_files    = spec.files.grep(%r{^spec/})
   spec.require_paths = ['lib']
--- lib/grover/processor.rb
+++ lib/grover/processor.rb
@@ -76,7 +76,7 @@ class Grover
       @package_json ||= JSON.parse(File.read(package_json_path))
     end
 
-    def call_js_method(method, url_or_html, options) # rubocop:disable Metrics/MethodLength
+    def call_js_method(method, url_or_html, options) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
       stdin.puts JSON.dump([method, url_or_html, options])
       input = stdout.gets
       raise Errno::EPIPE, "Can't read from worker" if input.nil?
@@ -90,6 +90,8 @@ class Grover
       else
         raise Grover::JavaScript.const_get(error_class, false), message
       end
+    rescue JSON::ParserError
+      raise Grover::Error, 'Malformed worker response'
     rescue Errno::EPIPE, IOError
       raise Grover::Error, "Worker process failed:\n#{stderr.read}"
     end
