2c22ab9d1715d08e725f170069ed0457f8bfee51
Micah Geisel <micah@botandrose.com>
2023-03-28 13:02:57 +0200

    split ferrum instantiation off into its own method, so that if it fails, the error doesn't get swallowed by subsequent nil-access in the ensure block.

--- lib/grover/ferrum_processor.rb
+++ lib/grover/ferrum_processor.rb
@@ -5,12 +5,17 @@ require "base64"
 class Grover
   class FerrumProcessor
     def convert(method, html, options)
-      browser = Ferrum::Browser.new
       page = browser.create_page
       page.content = html
       Base64.decode64(page.pdf)
     ensure
       browser.quit
+    end
+
+    private
+
+    def browser
+      @browser ||= Ferrum::Browser.new
     end
   end
 end
